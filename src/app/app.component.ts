6c35a47d1837587034b77b542597ef9b8f410df2
Ланцев Сергей <Gopotyzik@yandex.ru>
2024-03-28 16:36:01 +0300

    feat(app): настройки конфигурационный файлов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular_features_2.0';
+	title = 'Angular_features_2.0';
 }
